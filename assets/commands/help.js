95fe5f82554abe070176df3d4860552b48de7d24
Patrik Mintěl <ja@patrick115.eu>
2022-02-24 23:20:10 +0100

    Display admin commands only for admin

--- assets/commands/help.js
+++ assets/commands/help.js
@@ -7,13 +7,21 @@ module.exports = {
     execute: function (message, args) {
         let text = '-----===== **COMMANDS** ======-----'
         message.client.commands.forEach((command) => {
+            if (command?.admin && !message.author?.owner) return
             let line = `- ${message.client.config.prefix}${command.name}`
             if (command.arguments.length) {
                 command.arguments.forEach((argument) => {
                     line += ` <${argument}>`
                 })
             }
-            line += ` - ${command.description}`
+            if (command.description) {
+                line += ` - ${command.description}`
+            }
+
+            if (command?.admin) {
+                line += ` - **Admin Command**`
+            }
+
             if (!command.subcommands.length) {
                 text += `\n${line}`
                 return
