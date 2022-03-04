9d5810d2259c73ada2c659edaf7f16dd81f56ae8
Patrik Mintěl <ja@patrick115.eu>
2022-03-04 20:29:26 +0100

    Added comments to help command

--- assets/commands/help.js
+++ assets/commands/help.js
@@ -5,40 +5,57 @@ module.exports = {
     description: 'Displays all commands',
 
     execute: function (message, args) {
+        //Variable with text to send
         let text = '-----===== **COMMANDS** ======-----'
+
+        //foreach all loaded comamnds
         message.client.commands.forEach((command) => {
+            //if command command is for admin and user is not admin, then skip
             if (command?.admin && !message.author?.owner) return
+
+            //add comamnd with prefix to text
             let line = `- ${message.client.config.prefix}${command.name}`
+            //check if comamnd have arguments
             if (command.arguments.length) {
+                //loop arguments and add it to text
                 command.arguments.forEach((argument) => {
                     line += ` <${argument}>`
                 })
             }
+            //if command have description add it to text
             if (command.description) {
                 line += ` - ${command.description}`
             }
 
+            //if command is for admin add information text to end of command
             if (command?.admin) {
                 line += ` - **Admin Command**`
             }
 
+            //if command dont have any subcommands, then add a new line to text and continue
             if (!command.subcommands.length) {
                 text += `\n${line}`
                 return
             }
+            //if command have subcommands, then add colon to end of line
             line += `:`
+            //loop subcommands and add it to text
             command.subcommandsExec.forEach((subcommand) => {
                 line += `\n  - ${subcommand.name}`
+                //if subcommand have arguments, then add it to text
                 if (subcommand.arguments.length) {
                     subcommand.arguments.forEach((argument) => {
                         line += ` <${argument}>`
                     })
                 }
+                //if subcommand have description, then add it to text
                 if (subcommand.description) line += ` - ${subcommand.description}`
             })
+            //add subcommand to text
             text += `\n${line}`
         })
 
+        //reply to user message with text
         message.reply(text)
     },
 }
