3037eba594580c33256ecb5adc4f084d63d7f556
Patrik Mintěl <ja@patrick115.eu>
2022-03-04 20:35:18 +0100

    Added comments to restart comamnd

--- assets/commands/restart.js
+++ assets/commands/restart.js
@@ -5,8 +5,11 @@ module.exports = {
     description: 'Restart the bot',
     admin: true,
     execute: async function (message, args) {
+        //send text to console
         console.log('✔️  Restarting...')
+        //reply to user with text
         await message.reply('Restarting...')
+        //exit process
         process.exit(0)
     },
 }
