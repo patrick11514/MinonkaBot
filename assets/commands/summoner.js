726761b63210f12a89d6d0ce31d6d3f3d0eda9dc
Patrik Mintěl <ja@patrick115.eu>
2022-03-06 04:47:11 +0100

    Beta summoner command

---
+++ assets/commands/summoner.js
@@ -0,0 +1,163 @@
+const { MessageActionRow, MessageButton } = require('discord.js')
+
+module.exports = {
+    name: 'summoner',
+    subcommands: [],
+    description: 'Get info about summoner',
+    arguments: ['name'],
+    setup: function (client, reload = false) {
+        if (reload) return
+
+        client.on('interactionCreate', async (interaction) => {
+            if (!interaction.isButton()) return
+            let id = interaction.customId
+            let split = id.split('@')
+            let fromCommand = split[0]
+            if (fromCommand != 'SUMMONER') return
+
+            let name = split[1]
+            let server = split[2]
+            let channelId = split[3]
+            let original_message_id = split[4]
+
+            let channel = await interaction.client.channels.fetch(channelId)
+            let message = await channel.messages.fetch(original_message_id)
+
+            if (message.author.id != interaction.user.id) {
+                return interaction.reply({ content: "You can't use this interaction!", ephemeral: true })
+            }
+
+            let components = interaction.message.components
+
+            components.forEach((row) => {
+                row.components.forEach((button) => {
+                    button.disabled = true
+                })
+            })
+
+            interaction.message.edit({ components: components })
+
+            //execute command summoner
+            interaction.client.commands.get('summoner').execute(message, [`${name}@${server}`])
+
+            //reply with client only message
+            interaction.reply({
+                content: `Executing command ${this.name} for player ${name} on server ${server}`,
+                ephemeral: true,
+            })
+        })
+    },
+    execute: async function (message, args) {
+        //set constant config to config from global scope
+        const config = message.client.config
+        //set constant global function to functions from global scope
+        const gf = message.client.fc
+
+        let last_message = null
+
+        //if no arguments, then reply with error message
+        if (args.length < 1) {
+            return message.reply('Please provide a summoner name')
+        }
+        //put arguments together with space between them (if somebody have nickname with space)
+        let name = args.join(' ')
+        let region
+        let summoner = null
+
+        if (name.includes('@')) {
+            //split name and region
+            let split = name.split('@')
+            //and set name to first part
+            name = split[0]
+            //and set region to second part
+            region = split[1].toUpperCase()
+            //if region is invalid then reply with error message
+            if (!config.regions.includes(region)) {
+                let regions = config.regions.join(', ')
+                return message.reply(`Invalid region \`${region}\`.\nValid regions ${regions}`)
+            }
+        } else {
+            //try to search player on some region
+            let msg = await message.reply('Searching player')
+            let dots = 1
+            let loaded = false
+            let loop = async (msg) => {
+                await new Promise((resolve) => setTimeout(resolve, 800))
+                if (loaded) return
+                await msg.edit(`Searching player${'.'.repeat(dots)}`)
+                dots++
+                if (dots > 3) dots = 1
+                await loop(msg)
+            }
+            loop(msg)
+
+            let find = await gf.findSummoner(name)
+
+            loaded = true
+
+            //if player not found then reply with error message
+            if (!find) {
+                return message.reply("Can't find player on any server")
+            }
+
+            let regions = find.regions
+            let info = find.info
+
+            //if player found on more than 1 server then reply with message with list of servers
+            if (regions.length > 1) {
+                let rows = []
+                let player_info = '\n**Region: level**\n'
+
+                for (let [id, data] of Object.entries(info)) {
+                    player_info += `${id}: \`${data.summonerLevel}\` `
+                }
+
+                for (let i = 0, j = regions.length; i < j; i += 5) {
+                    let temp = regions.slice(i, i + 5)
+                    let row = new MessageActionRow()
+                    temp.forEach((region) => {
+                        row.addComponents(
+                            new MessageButton()
+                                .setCustomId(
+                                    `SUMMONER@${name}@${region.toLowerCase()}@${message.channelId}@${message.id}`
+                                )
+                                .setLabel(region)
+                                .setStyle('PRIMARY')
+                        )
+                    })
+                    rows.push(row)
+                }
+
+                return msg.edit({
+                    content: `Found ${name} on multiple servers, please select one.\n${player_info}`,
+                    components: rows,
+                })
+            }
+
+            region = regions[0]
+            summoner = info[0]
+        }
+
+        if (summoner == null) {
+            summoner = await gf.getSummoner(name, region)
+        }
+
+        if (!summoner) {
+            return message.reply(`Summoner \`${name}\` on region \`${region}\` not found`)
+        }
+
+        let info = {
+            name: summoner.name,
+            region: region,
+            level: summoner.summonerLevel,
+            icon: summoner.profileIconId,
+        }
+
+        let ranked_data = await gf.getSummonerRanks(summoner.id, region)
+
+        info.solo = ranked_data.solo
+        info.flex = ranked_data.flex
+        console.log(info)
+        let getImage = await gf.getImage('summoner', info)
+    },
+}
