3d03471e2db2abf3f3e7a8379378f9a13e1f87d3
Patrik Mintěl <ja@patrick115.eu>
2022-03-19 17:33:23 +0100

    Přidány komentáře

--- assets/commands/subcommands/clash@getTeam.js
+++ assets/commands/subcommands/clash@getTeam.js
@@ -1,9 +1,15 @@
+const { Message } = require('discord.js')
+
 module.exports = {
     mainCommand: 'clash',
     name: 'getTeam',
     arguments: [],
     description: 'Get clash team of player',
-
+    /**
+     *
+     * @param {Message} message
+     * @param {Array} args
+     */
     execute: function (message, args) {
         message.reply('TEST')
     },
