ce43f9c561e0ce84aef1adf44ea9b853611d8c37
Patrik Mintěl <ja@patrick115.eu>
2022-04-24 19:04:50 +0200

    Edited default info

---
+++ assets/commands/mastery.js
@@ -0,0 +1,23 @@
+const { Client, Message } = require('discord.js')
+
+module.exports = {
+    name: 'mastery',
+    subcommands: ['champion'],
+    description: 'Show masteries of champions',
+    arguments: ['name', 'region'],
+    /**
+     * Optional function
+     * @param {Client} client
+     */
+    setup: function (client) {
+        //do something
+    },
+    /**
+     * Required function
+     * @param {Message} message
+     * @param {Array} args
+     */
+    execute: async function (message, args) {
+        //do something
+    },
+}
