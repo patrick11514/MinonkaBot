486c245f45fc6f002584de11d4a3e44f64a68dc4
Patrik Mintěl <ja@patrick115.eu>
2022-04-24 22:40:34 +0200

    Added interaction respond event

--- assets/commands/mastery.js
+++ assets/commands/mastery.js
@@ -1,6 +1,7 @@
 const { Client, Message } = require('discord.js')
 const FindUser = require('../functions/findUser')
 const fs = require('fs')
+const SendComponent = require('../functions/sendComponents')
 
 module.exports = {
     name: 'mastery',
@@ -11,8 +12,39 @@ module.exports = {
      * Optional function
      * @param {Client} client
      */
-    setup: function (client) {
-        //do something
+    setup: function (client, reload = false) {
+        if (reload) return
+
+        client.on('interactionCreate', async (interaction) => {
+            if (!interaction.isButton()) return
+            let id = interaction.customId
+            let split = id.split('@')
+            let fromCommand = split[0]
+            if (fromCommand != 'MASTERY') return
+
+            let name = split[1]
+            let server = split[2]
+            let channelId = split[3]
+            let original_message_id = split[4]
+
+            let channel = await interaction.client.channels.fetch(channelId)
+            let message = await channel.messages.fetch(original_message_id)
+
+            if (message.author.id != interaction.user.id) {
+                return interaction.reply({ content: "You can't use this interaction!", ephemeral: true })
+            }
+
+            interaction.message.edit({ components: [] })
+
+            //execute command summoner
+            interaction.client.commands.get('mastery').execute(message, [`${name}@${server}`], interaction.message)
+
+            //reply with client only message
+            interaction.reply({
+                content: `Executing command ${this.name} for player ${name} on server ${server}`,
+                ephemeral: true,
+            })
+        })
     },
     /**
      * Required function
