072f9e22983068ba3acf0dad72026adf8a027201
Patrik Mintěl <ja@patrick115.eu>
2022-05-10 01:47:18 +0200

    Added servers with emojis

--- assets/commands/generateemojis.js
+++ assets/commands/generateemojis.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 module.exports = {
     name: 'generateemojis',
-    subcommands: ["show"],
+    subcommands: ["show", "showraw"],
     description: 'Generate emojis list',
     arguments: [],
     admin: true,
@@ -12,7 +12,19 @@ module.exports = {
      * @param {Message} message
      */
     execute: async function (message) {
-        let servers = ['955054979192881162', '955053883103780894', '955054220766248970', '955054457098498078', "967816629557817354", "969587984682877048"]
+        let servers = [
+            '955054979192881162', //Champs1
+            '955053883103780894', //Champs2
+            '955054220766248970', //Champs3
+            '955054457098498078', //Champs4
+            '967816629557817354', //Mix1
+            '969587984682877048', //Mix2
+            '973334813467611146', //Items1
+            '973338458003222568', //Items2
+            '973338484779647077', //Items3
+            '973338509458964590', //Items4
+            '973338537007128576', //Items5
+        ]
         let count = 0
         let emojiList = {}
 
