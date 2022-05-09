7c21d1744c8cb29a59c4be7a7dded62aa117ddb7
Patrik Mintěl <ja@patrick115.eu>
2022-05-10 01:47:52 +0200

    Added subcommand to show only emojis without text

---
+++ assets/commands/subcommands/generateemojis@showraw.js
@@ -0,0 +1,40 @@
+const { Message } = require('discord.js')
+const fs = require('fs')
+
+module.exports = {
+    mainCommand: 'generatemojis',
+    name: 'showraw',
+    arguments: [],
+    description: 'Show all loaded emojis',
+    /**
+     *
+     * @param {Message} message
+     */
+    execute: async function (message) {
+        let emojis = JSON.parse(await fs.readFileSync('./assets/emojis.json'))
+
+        let texts = []
+        let text = ""
+        for (let emoji in emojis) {
+            if (text.length + emojis[emoji].length > 2000) {
+                texts.push(text)
+                text = ""
+            }
+            text += `${emojis[emoji]}`
+        }
+        if (text.length) {
+            texts.push(text)
+        }
+
+        let reply = false
+
+        for (let text of texts) {
+            if (!reply) {
+                await message.reply(text)
+                reply = true
+            } else {
+                await message.channel.send(text)
+            }
+        }
+    },
+}
