54aa888b34f88b35774283474929c907fd32434b
patrick11514 <ja@patrick115.eu>
2022-10-08 21:52:40 +0200

    Account picker done

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -1,4 +1,5 @@
-import { Client, CommandInteraction } from 'discord.js'
+import { ButtonInteraction, Client, CommandInteraction } from 'discord.js'
+import accountPicker from '../components/accountPicker'
 import Images from '../lib/images/core'
 import Logger from '../lib/logger'
 import Riot from '../lib/riot/core'
@@ -16,84 +17,96 @@ export default (client: Client) => {
             generateProfile(username?.value as string, region?.value as string, interaction)
         }
     })
+}
 
-    async function generateProfile(
-        username: string | null,
-        region: string | null,
-        interaction: CommandInteraction,
-        reply = false
-    ) {
-        let userData: {
-            username: string | null
-            region: string | null
-        } = {
-            username: null,
-            region: null,
-        }
+export async function generateProfile(
+    username: string | null,
+    region: string | null,
+    interaction: CommandInteraction | ButtonInteraction,
+    edit = true
+) {
+    let userData: {
+        username: string | null
+        region: string | null
+    } = {
+        username: null,
+        region: null,
+    }
 
-        if (!username) {
-            //tbd
-        } else {
-            let riot = new Riot()
+    if (!username) {
+        //tbd
+    } else {
+        let riot = new Riot()
 
-            if (region) {
-                userData.username = username
-                userData.region = region
+        if (region) {
+            userData.username = username
+            userData.region = region
 
-                let data = await riot.getSummonerByName(userData.username, userData.region)
+            let data = await riot.getSummonerByName(userData.username, userData.region)
 
-                if (!data) {
-                    return interaction.reply({
+            if (!data) {
+                if (edit) {
+                    interaction.editReply({
+                        content: 'Jméno :user nebylo nalezeno na serveru :region!'
+                            .replace(':user', userData.username)
+                            .replace(':region', userData.region),
+                    })
+                } else {
+                    interaction.reply({
                         content: 'Jméno :user nebylo nalezeno na serveru :region!'
                             .replace(':user', userData.username)
                             .replace(':region', userData.region),
                     })
                 }
+                return
+            }
 
-                let challenges = await riot.getChallenges(data.puuid, userData.region)
+            let challenges = await riot.getChallenges(data.puuid, userData.region)
 
-                if (!challenges) {
-                    return interaction.reply({
+            if (!challenges) {
+                if (edit) {
+                    interaction.editReply({ content: 'Nepovedlo se načíst data z Riot API!' })
+                } else {
+                    interaction.reply({
                         content: 'Nepovedlo se načíst data z Riot API!',
                     })
                 }
+                return
+            }
 
-                if (!reply) await interaction.deferReply()
+            if (edit) await interaction.deferReply()
 
-                let dataFor = {
-                    username: data.name,
-                    level: data.summonerLevel,
-                    iconId: data.profileIconId,
-                    title: challenges.preferences.title,
-                    challenges: challenges.preferences.challengeIds?.map((id) => {
-                        let challenge = (challenges as UserChallenges).challenges.find(
-                            (chall) => chall.challengeId === id
-                        )
-                        if (!challenge) return { id: 0, tier: '' }
+            let dataFor = {
+                username: data.name,
+                level: data.summonerLevel,
+                iconId: data.profileIconId,
+                title: challenges.preferences.title,
+                challenges: challenges.preferences.challengeIds?.map((id) => {
+                    let challenge = (challenges as UserChallenges).challenges.find((chall) => chall.challengeId === id)
+                    if (!challenge) return { id: 0, tier: '' }
 
-                        return {
-                            id: id,
-                            tier: challenge.level,
-                        }
-                    }),
-                }
+                    return {
+                        id: id,
+                        tier: challenge.level,
+                    }
+                }),
+            }
 
-                let images = new Images()
+            let images = new Images()
 
-                let image = await images.generateProfilePicture(dataFor)
+            let image = await images.generateProfilePicture(dataFor)
 
-                interaction.editReply({ content: '', files: [image] })
+            interaction.editReply({ content: '', files: [image] })
+        } else {
+            interaction.reply('Nezadal jsi region, bude to chvíli trvat...')
+
+            let accountData = await riot.findAccount(username)
+
+            if (accountData.length > 1) {
+                new accountPicker(accountData, interaction, true).bindFunction('profile').send()
             } else {
-                interaction.reply('Nezadal jsi region, bude to chvíli trvat...')
-
-                let accountData = await riot.findAccount(username)
-
-                if (accountData.length > 1) {
-                    //account picker
-                } else {
-                    interaction.editReply({ content: 'Máme tvůj účet! Nyní získáváme data o něm...' })
-                    await generateProfile(accountData[0].name, accountData[0].region, interaction, true)
-                }
+                interaction.editReply({ content: 'Máme tvůj účet! Nyní získáváme data o něm...' })
+                await generateProfile(accountData[0].name, accountData[0].region, interaction, false)
             }
         }
     }
--- src/components/accountPicker.ts
+++ src/components/accountPicker.ts
@@ -1,15 +1,104 @@
-import { Client, Interaction } from 'discord.js'
+import {
+    ActionRow,
+    ActionRowBuilder,
+    AnyComponentBuilder,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
+    Client,
+    CommandInteraction,
+    MessageActionRowComponent,
+} from 'discord.js'
+import crypto from 'crypto'
+import { generateProfile } from '../commands/profile'
 
 class accountPicker {
-    construct(
+    accounts: Array<{
+        name: string
+        region: string
+    }> = []
+
+    id: string = ''
+    rows: Array<ActionRowBuilder<ButtonBuilder>> = []
+    interaction: CommandInteraction | ButtonInteraction = null as unknown as CommandInteraction
+    message: string = ''
+    edit: boolean = false
+
+    constructor(
         accounts: Array<{
             name: string
             region: string
         }>,
-        client: Client,
-        interaction: Interaction,
-        edit: boolean = false
-    ) {}
+        interaction: CommandInteraction | ButtonInteraction,
+        edit = false,
+        message = 'Zde je seznam nalezených účtů:'
+    ) {
+        //generate buttons
+        let rows = []
+        let accs = []
+
+        this.accounts = accounts
+
+        //divide accounts to arrays by five
+        while (accounts.length > 0) {
+            accs.push(accounts.slice(0, 5))
+            accounts = accounts.slice(5)
+        }
+
+        let key = crypto.randomBytes(4).toString('hex')
+        this.id = key
+
+        let acc = 0
+        for (let accList of accs) {
+            let row = new ActionRowBuilder<ButtonBuilder>()
+
+            for (let account of accList) {
+                row.addComponents(
+                    new ButtonBuilder()
+                        .setLabel(`${account.name} (${process.client.config.regionTranslates[account.region]})`)
+                        .setStyle(ButtonStyle.Primary)
+                        .setCustomId(key + '@' + acc)
+                )
+                acc++
+            }
+            rows.push(row)
+        }
+
+        this.rows = rows
+        this.interaction = interaction
+        this.message = message
+        this.edit = edit
+        return this
+    }
+
+    bindFunction(name: string) {
+        process.client.emitter.on('button', async (interaction: ButtonInteraction) => {
+            let idData = interaction.customId.split('@')
+            if (idData.length != 2) return
+            let id = idData[0]
+            let acc = idData[1]
+            if (id != this.id) return
+            let account = this.accounts[parseInt(acc)]
+
+            await interaction.reply({ content: 'Účet vybrán, nyní provádníme další akce...', ephemeral: true })
+            await this.interaction.editReply({ content: 'Načítání...', components: [] })
+
+            switch (name) {
+                case 'profile': {
+                    generateProfile(account.name, account.region, this.interaction, false)
+                    break
+                }
+            }
+        })
+        return this
+    }
+    send() {
+        if (this.edit) {
+            this.interaction.editReply({ content: `${this.message}`, components: this.rows })
+        } else {
+            this.interaction.reply({ content: `${this.message}`, components: this.rows })
+        }
+    }
 }
 
 export default accountPicker
