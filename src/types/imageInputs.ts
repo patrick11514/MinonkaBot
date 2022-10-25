1d407696e24aa55b24366d7964a497c31e324ce7
patrick11514 <ja@patrick115.eu>
2022-10-25 09:32:44 +0200

    Types for inputs to image functions

---
+++ src/types/imageInputs.ts
@@ -0,0 +1,35 @@
+import { EncryptedSummonerId, QueueTypes, Ranks, Tiers } from './riotApi'
+
+interface profilePicture {
+    username: string
+    level: number
+    iconId: number
+    title: string
+    challenges: Array<{
+        id: number
+        tier: string
+    }>
+}
+
+interface rankedProfile {
+    profileIconId: number
+    summonerName: string
+    level: number
+    rankeds: Array<{
+        leagueId: string
+        queueType: QueueTypes
+        tier: Tiers
+        rank: Ranks
+        summonerId: EncryptedSummonerId
+        summonerName: string
+        leaguePoints: number
+        wins: number
+        losses: number
+        veteran: boolean
+        inactive: boolean
+        freshBlood: boolean
+        hotStreak: boolean
+    }>
+}
+
+export { profilePicture, rankedProfile }
