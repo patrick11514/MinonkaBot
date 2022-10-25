247abe0b9a045a009958dc29a5a7f488071a16c9
patrick11514 <ja@patrick115.eu>
2022-10-25 10:50:16 +0200

    Added function firstUpper

--- src/lib/riot/utilities.ts
+++ src/lib/riot/utilities.ts
@@ -125,6 +125,10 @@ class Utilities {
             | 'GRANDMASTER'
             | 'CHALLENGER'
     }
+
+    firstUpper(s: string) {
+        return s.charAt(0).toUpperCase() + s.slice(1)
+    }
 }
 
 export default new Utilities()
