4527c79e3f5a00cc022702c544d07e8d0c9a56c5
patrick11514 <ja@patrick115.eu>
2022-10-27 17:10:39 +0200

    Moved commands to separated file

---
+++ src/commands.ts
@@ -0,0 +1,180 @@
+const regions = [
+    { name: 'EUNE', value: 'EUN1' },
+    { name: 'EUW', value: 'EUW1' },
+    { name: 'BR', value: 'BR1' },
+    { name: 'JP', value: 'JP1' },
+    { name: 'KR', value: 'KR' },
+    { name: 'LAN', value: 'LA1' },
+    { name: 'LAS', value: 'LA2' },
+    { name: 'NA', value: 'NA1' },
+    { name: 'OCE', value: 'OC1' },
+    { name: 'TR', value: 'TR1' },
+    { name: 'RU', value: 'RU' },
+]
+
+let commands: {
+    [key: string]: {
+        description: string
+        options: Array<{
+            name: string
+            description: string
+            required: boolean
+            choices?: Array<{
+                name: string
+                description?: string
+                value?: string
+            }>
+        }>
+    }
+} = {
+    profile: {
+        description: 'Zobrazí tvůj league of legends profil',
+        options: [
+            {
+                name: 'username',
+                description: 'Jméno vyvolávače',
+                required: false,
+            },
+            {
+                name: 'region',
+                description: 'Region, na kterém je tvůj účet vytvořen. (Zrychlí vyhledání účtu)',
+                required: false,
+                choices: regions,
+            },
+        ],
+    },
+    language: {
+        description: 'Změní jazyk riot API (později i bota)',
+        options: [
+            {
+                name: 'language',
+                description: 'Jazyk, na který chceš změnit bota',
+                required: false,
+                choices: [
+                    { value: 'cs_CZ', name: 'Czech (Czech Republic)' },
+                    { value: 'el_GR', name: 'Greek (Greece)' },
+                    { value: 'pl_PL', name: 'Polish (Poland)' },
+                    { value: 'ro_RO', name: 'Romanian (Romania)' },
+                    { value: 'hu_HU', name: 'Hungarian (Hungary)' },
+                    { value: 'en_GB', name: 'English (United Kingdom)' },
+                    { value: 'de_DE', name: 'German (Germany)' },
+                    { value: 'es_ES', name: 'Spanish (Spain)' },
+                    { value: 'it_IT', name: 'Italian (Italy)' },
+                    { value: 'fr_FR', name: 'French (France)' },
+                    { value: 'ja_JP', name: 'Japanese (Japan)' },
+                    { value: 'ko_KR', name: 'Korean (Korea)' },
+                    { value: 'es_MX', name: 'Spanish (Mexico)' },
+                    { value: 'es_AR', name: 'Spanish (Argentina)' },
+                    { value: 'pt_BR', name: 'Portuguese (Brazil)' },
+                    { value: 'en_US', name: 'English (United States)' },
+                    { value: 'en_AU', name: 'English (Australia)' },
+                    { value: 'ru_RU', name: 'Russian (Russia)' },
+                    { value: 'tr_TR', name: 'Turkish (Turkey)' },
+                    { value: 'ms_MY', name: 'Malay (Malaysia)' },
+                    { value: 'en_PH', name: 'English (Republic of the Philippines)' },
+                    { value: 'en_SG', name: 'English (Singapore)' },
+                ],
+            },
+            {
+                name: 'language2',
+                description: 'Jazyk, na který chceš změnit bota',
+                required: false,
+                choices: [
+                    { value: 'th_TH', name: 'Thai (Thailand)' },
+                    { value: 'vn_VN', name: 'Vietnamese (Viet Nam)' },
+                    { value: 'id_ID', name: 'Indonesian (Indonesia)' },
+                    { value: 'zh_MY', name: 'Chinese (Malaysia)' },
+                    { value: 'zh_CN', name: 'Chinese (China)' },
+                    { value: 'zh_TW', name: 'Chinese (Taiwan)' },
+                ],
+            },
+        ],
+    },
+    link: {
+        description: 'Připojí tvůj discord účet k tvému league of legends účtu',
+        options: [
+            {
+                name: 'action',
+                description: 'Akce, kterou chceš provést',
+                required: true,
+                choices: [
+                    {
+                        name: 'list',
+                        description: 'Zobrazí všechny tvé připojené účty',
+                    },
+                    {
+                        name: 'add',
+                        description: 'Připojí tvůj league of legends účet k tvému discord účtu',
+                    },
+                    {
+                        name: 'delete',
+                        description: 'Odpojí tvůj league of legends účet od tvého discord účtu',
+                    },
+                ],
+            },
+            {
+                name: 'username',
+                description: 'Jméno vyvolávače',
+                required: false,
+            },
+            {
+                name: 'region',
+                description: 'Region, na kterém je tvůj účet vytvořen. (Zrychlí vyhledání účtu)',
+                required: false,
+                choices: regions,
+            },
+        ],
+    },
+    rank: {
+        description: 'Zobrazí tvůj league of legends rank',
+        options: [
+            {
+                name: 'username',
+                description: 'Jméno vyvolávače',
+                required: false,
+            },
+            {
+                name: 'region',
+                description: 'Region, na kterém je tvůj účet vytvořen. (Zrychlí vyhledání účtu)',
+                required: false,
+                choices: regions,
+            },
+        ],
+    },
+    namehistory: {
+        description: 'Zobrazí historii tvého jména (historie se updatuje pouze po vykonanání nějakého příkazu)',
+        options: [
+            {
+                name: 'username',
+                description: 'Jméno vyvolávače',
+                required: false,
+            },
+            {
+                name: 'region',
+                description: 'Region, na kterém je tvůj účet vytvořen. (Zrychlí vyhledání účtu)',
+                required: false,
+                choices: regions,
+            },
+        ],
+    },
+    help: {
+        description: 'Zobrazí seznam všech příkazů',
+        options: [
+            {
+                name: 'command',
+                description: 'Příkaz, který chceš zobrazit',
+                required: false,
+                choices: [],
+            },
+        ],
+    },
+}
+
+commands.help.options[0].choices = Object.keys(commands).map((command: string) => {
+    return {
+        name: command,
+        description: commands[command].description,
+    }
+})
+
+export default commands
--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -1,25 +1,12 @@
 import { REST, SlashCommandBuilder, Routes } from 'discord.js'
 import * as dotenv from 'dotenv'
+import commands from './commands'
 dotenv.config()
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN)
 
 const removeCommands = false
 const guildsIds = ['713520402315608148']
-
-const regions = [
-    { name: 'EUNE', value: 'EUN1' },
-    { name: 'EUW', value: 'EUW1' },
-    { name: 'BR', value: 'BR1' },
-    { name: 'JP', value: 'JP1' },
-    { name: 'KR', value: 'KR' },
-    { name: 'LAN', value: 'LA1' },
-    { name: 'LAS', value: 'LA2' },
-    { name: 'NA', value: 'NA1' },
-    { name: 'OCE', value: 'OC1' },
-    { name: 'TR', value: 'TR1' },
-    { name: 'RU', value: 'RU' },
-]
 
 ;(async () => {
     if (removeCommands) {
@@ -47,110 +34,37 @@ const regions = [
     } else {
         const rawCommands: Array<any> = []
 
-        //new commands
-        rawCommands.push(
-            new SlashCommandBuilder()
-                .setName('profile')
-                .setDescription('Zobrazí tvůj league of legends profil')
-                .addStringOption((option) => option.setName('username').setDescription('Jméno vyvolávače'))
-                .addStringOption((option) =>
-                    option
-                        .setName('region')
-                        .setDescription('Region, na kterém je tvůj účet vytvořen. (Zrychlí vyhledání účtu)')
-                        .addChoices(...regions)
-                )
-        )
+        for (let command in commands) {
+            let commandData = commands[command]
+            let builder = new SlashCommandBuilder().setName(command).setDescription(commandData.description)
 
-        rawCommands.push(
-            new SlashCommandBuilder()
-                .setName('language')
-                .setDescription('Změní jazyk riot API (později i bota)')
-                .addStringOption((option) =>
-                    option
-                        .setName('language')
-                        .setDescription('Jazyk, na který chceš změnit bota')
-                        .addChoices(
-                            { value: 'cs_CZ', name: 'Czech (Czech Republic)' },
-                            { value: 'el_GR', name: 'Greek (Greece)' },
-                            { value: 'pl_PL', name: 'Polish (Poland)' },
-                            { value: 'ro_RO', name: 'Romanian (Romania)' },
-                            { value: 'hu_HU', name: 'Hungarian (Hungary)' },
-                            { value: 'en_GB', name: 'English (United Kingdom)' },
-                            { value: 'de_DE', name: 'German (Germany)' },
-                            { value: 'es_ES', name: 'Spanish (Spain)' },
-                            { value: 'it_IT', name: 'Italian (Italy)' },
-                            { value: 'fr_FR', name: 'French (France)' },
-                            { value: 'ja_JP', name: 'Japanese (Japan)' },
-                            { value: 'ko_KR', name: 'Korean (Korea)' },
-                            { value: 'es_MX', name: 'Spanish (Mexico)' },
-                            { value: 'es_AR', name: 'Spanish (Argentina)' },
-                            { value: 'pt_BR', name: 'Portuguese (Brazil)' },
-                            { value: 'en_US', name: 'English (United States)' },
-                            { value: 'en_AU', name: 'English (Australia)' },
-                            { value: 'ru_RU', name: 'Russian (Russia)' },
-                            { value: 'tr_TR', name: 'Turkish (Turkey)' },
-                            { value: 'ms_MY', name: 'Malay (Malaysia)' },
-                            { value: 'en_PH', name: 'English (Republic of the Philippines)' },
-                            { value: 'en_SG', name: 'English (Singapore)' }
-                        )
-                )
-                .addStringOption((option) =>
-                    option
-                        .setName('language2')
-                        .setDescription('Jazyk, na který chceš změnit bota')
-                        .addChoices(
-                            { value: 'th_TH', name: 'Thai (Thailand)' },
-                            { value: 'vn_VN', name: 'Vietnamese (Viet Nam)' },
-                            { value: 'id_ID', name: 'Indonesian (Indonesia)' },
-                            { value: 'zh_MY', name: 'Chinese (Malaysia)' },
-                            { value: 'zh_CN', name: 'Chinese (China)' },
-                            { value: 'zh_TW', name: 'Chinese (Taiwan)' }
-                        )
-                )
-        )
+            if (commandData.options) {
+                for (let option of commandData.options) {
+                    builder.addStringOption((o) => {
+                        let opt = o.setName(option.name).setDescription(option.description).setRequired(option.required)
+                        if (option.choices) {
+                            opt.addChoices(
+                                ...option.choices.map((choice) => {
+                                    return {
+                                        name: choice.name,
+                                        value: choice.value || choice.name,
+                                    }
+                                })
+                            )
+                        }
+                        return opt
+                    })
+                }
+            }
 
-        rawCommands.push(
-            new SlashCommandBuilder()
-                .setName('link')
-                .setDescription('Linkuje tvůj discord účet s tvým league of legends účtem')
-                .addStringOption((option) =>
-                    option
-                        .setName('action')
-                        .setDescription('Akce, kterou chceš provést')
-                        .setRequired(true)
-                        .addChoices(
-                            { name: 'list', value: 'list' },
-                            { name: 'add', value: 'add' },
-                            { name: 'delete', value: 'delete' }
-                        )
-                )
-                .addStringOption((option) => option.setName('username').setDescription('Jméno vyvolávače'))
-                .addStringOption((option) =>
-                    option
-                        .setName('region')
-                        .setDescription('Region, na kterém je tvůj účet vytvořen. (Zrychlí vyhledání účtu)')
-                        .addChoices(...regions)
-                )
-        )
-
-        rawCommands.push(
-            new SlashCommandBuilder()
-                .setName('rank')
-                .setDescription('Zobrazí tvůj league of legends rank')
-                .addStringOption((option) => option.setName('username').setDescription('Jméno vyvolávače'))
-                .addStringOption((option) =>
-                    option
-                        .setName('region')
-                        .setDescription('Region, na kterém je tvůj účet vytvořen. (Zrychlí vyhledání účtu)')
-                        .addChoices(...regions)
-                )
-        )
+            rawCommands.push(builder)
+        }
 
         //
 
-        const commands = rawCommands.map((command) => command.toJSON())
+        const commandsJson = rawCommands.map((command) => command.toJSON())
 
-        rest.put(Routes.applicationCommands(process.env.DISCORD_ID), { body: commands })
+        rest.put(Routes.applicationCommands(process.env.DISCORD_ID), { body: commandsJson })
             .then(() => console.log(`Successfully registered all commands.`))
             .catch(console.error)
     }
