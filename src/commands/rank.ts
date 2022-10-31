2796eca617234ccd3ab33ac75c97bdcb1b572960
patrick11514 <ja@patrick115.eu>
2022-10-31 12:56:43 +0100

    Added check for account history to every command

--- src/commands/rank.ts
+++ src/commands/rank.ts
@@ -84,6 +84,21 @@ export async function generateRank(
                 return
             }
 
+            //save check history of account
+            let accounts = new linkedAccounts(
+                interaction.user.id,
+                interaction.client.usersDB,
+                interaction.client.nameHistoryDB
+            )
+
+            accounts.checkHistory([
+                {
+                    username: data.name,
+                    id: data.id,
+                    region: region,
+                },
+            ])
+
             //get image
             let images = new Images()
 
