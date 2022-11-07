a8b1fd64232358b120822396352fd96d46ee1cba
patrick11514 <ja@patrick115.eu>
2022-11-07 18:58:22 +0100

    Added types for match and errorResponse

--- src/types/riotApi.ts
+++ src/types/riotApi.ts
@@ -231,6 +231,272 @@ interface itemsData extends JSONRiotFiles {
     }
 }
 
+interface errorResponse {
+    status: {
+        message: string
+        status_code: number
+    }
+}
+
+interface matchChallenges {
+    '12AssistStreakCount': number
+    abilityUses: number
+    acesBefore15Minutes: number
+    alliedJungleMonsterKills: number
+    baronTakedowns: number
+    blastConeOppositeOpponentCount: number
+    bountyGold: number
+    buffsStolen: number
+    completeSupportQuestInTime: number
+    controlWardsPlaced: number
+    damagePerMinute: number
+    damageTakenOnTeamPercentage: number
+    dancedWithRiftHerald: number
+    deathsByEnemyChamps: number
+    dodgeSkillShotsSmallWindow: number
+    doubleAces: number
+    dragonTakedowns: number
+    earlyLaningPhaseGoldExpAdvantage: number
+    effectiveHealAndShielding: number
+    elderDragonKillsWithOpposingSoul: number
+    elderDragonMultikills: number
+    enemyChampionImmobilizations: number
+    enemyJungleMonsterKills: number
+    epicMonsterKillsNearEnemyJungler: number
+    epicMonsterKillsWithin30SecondsOfSpawn: number
+    epicMonsterSteals: number
+    epicMonsterStolenWithoutSmite: number
+    flawlessAces: number
+    fullTeamTakedown: number
+    gameLength: number
+    getTakedownsInAllLanesEarlyJungleAsLaner: number
+    goldPerMinute: number
+    hadOpenNexus: number
+    immobilizeAndKillWithAlly: number
+    initialBuffCount: number
+    initialCrabCount: number
+    jungleCsBefore10Minutes: number
+    junglerTakedownsNearDamagedEpicMonster: number
+    kTurretsDestroyedBeforePlatesFall: number
+    kda: number
+    killAfterHiddenWithAlly: number
+    killParticipation: number
+    killedChampTookFullTeamDamageSurvived: number
+    killsNearEnemyTurret: number
+    killsOnOtherLanesEarlyJungleAsLaner: number
+    killsOnRecentlyHealedByAramPack: number
+    killsUnderOwnTurret: number
+    killsWithHelpFromEpicMonster: number
+    knockEnemyIntoTeamAndKill: number
+    landSkillShotsEarlyGame: number
+    laneMinionsFirst10Minutes: number
+    laningPhaseGoldExpAdvantage: number
+    legendaryCount: number
+    lostAnInhibitor: number
+    maxCsAdvantageOnLaneOpponent: number
+    maxKillDeficit: number
+    maxLevelLeadLaneOpponent: number
+    moreEnemyJungleThanOpponent: number
+    multiKillOneSpell: number
+    multiTurretRiftHeraldCount: number
+    multikills: number
+    multikillsAfterAggressiveFlash: number
+    mythicItemUsed: number
+    outerTurretExecutesBefore10Minutes: number
+    outnumberedKills: number
+    outnumberedNexusKill: number
+    perfectDragonSoulsTaken: number
+    perfectGame: number
+    pickKillWithAlly: number
+    playedChampSelectPosition: number
+    poroExplosions: number
+    quickCleanse: number
+    quickFirstTurret: number
+    quickSoloKills: number
+    riftHeraldTakedowns: number
+    saveAllyFromDeath: number
+    scuttleCrabKills: number
+    skillshotsDodged: number
+    skillshotsHit: number
+    snowballsHit: number
+    soloBaronKills: number
+    soloKills: number
+    stealthWardsPlaced: number
+    survivedSingleDigitHpCount: number
+    survivedThreeImmobilizesInFight: number
+    takedownOnFirstTurret: number
+    takedowns: number
+    takedownsAfterGainingLevelAdvantage: number
+    takedownsBeforeJungleMinionSpawn: number
+    takedownsFirstXMinutes: number
+    takedownsInAlcove: number
+    takedownsInEnemyFountain: number
+    teamBaronKills: number
+    teamDamagePercentage: number
+    teamElderDragonKills: number
+    teamRiftHeraldKills: number
+    threeWardsOneSweeperCount: number
+    tookLargeDamageSurvived: number
+    turretPlatesTaken: number
+    turretTakedowns: number
+    turretsTakenWithRiftHerald: number
+    twentyMinionsIn3SecondsCount: number
+    unseenRecalls: number
+    visionScoreAdvantageLaneOpponent: number
+    visionScorePerMinute: number
+    wardTakedowns: number
+    wardTakedownsBefore20M: number
+    wardsGuarded: number
+}
+
+interface match {
+    metdata: {
+        dataVersion: string
+        matchId: string
+        participants: Array<EncryptedPuuid>
+    }
+    tournamentCode: string
+    info: {
+        gameCreation: number
+        gameDuration: number
+        gameStartTimestamp: number
+        gameEndTimestamp: number
+        gameId: number
+        gameMode: string
+        gameName: string
+        gameType: string
+        gameVersion: string
+        mapId: number
+        participants: Array<{
+            assists: number
+            baronKills: number
+            basicPings: number
+            bountyLevel: number
+            challenges: matchChallenges
+            champExperience: number
+            champLevel: number
+            championId: number
+            championName: string
+            championTransform: number
+            consumablesPurchased: number
+            damageDealtToBuildings: number
+            damageDealtToObjectives: number
+            damageDealtToTurrets: number
+            damageSelfMitigated: number
+            deaths: number
+            detectorWardsPlaced: number
+            doubleKills: number
+            dragonKills: number
+            eligibleForProgression: boolean
+            firstBloodAssist: boolean
+            firstBloodKill: boolean
+            firstTowerAssist: boolean
+            firstTowerKill: boolean
+            gameEndedInEarlySurrender: boolean
+            gameEndedInSurrender: boolean
+            goldEarned: number
+            goldSpent: number
+            individualPosition: string
+            inhibitorKills: number
+            inhibitorTakedowns: number
+            inhibitorsLost: number
+            item0: number
+            item1: number
+            item2: number
+            item3: number
+            item4: number
+            item5: number
+            item6: number
+            itemsPurchased: number
+            killingSprees: number
+            kills: number
+            lane: string
+            largestCriticalStrike: number
+            largestKillingSpree: number
+            largestMultiKill: number
+            longestTimeSpentLiving: number
+            magicDamageDealt: number
+            magicDamageDealtToChampions: number
+            magicDamageTaken: number
+            neutralMinionsKilled: number
+            nexusKills: number
+            nexusLost: number
+            nexusTakedowns: number
+            objectivesStolen: number
+            objectivesStolenAssists: number
+            participantId: number
+            pentaKills: number
+            perks: {
+                statPerks: {
+                    defense: number
+                    flex: number
+                    offense: number
+                }
+                styles: Array<{
+                    description: string
+                    selections: Array<{
+                        perk: number
+                        var1: number
+                        var2: number
+                        var3: number
+                    }>
+
+                    style: number
+                }>
+            }
+            physicalDamageDealt: number
+            physicalDamageDealtToChampions: number
+            physicalDamageTaken: number
+            profileIcon: number
+            puuid: EncryptedPuuid
+            quadraKills: number
+            riotIdName: string
+            riotIdTagline: string
+            role: string
+            sightWardsBoughtInGame: number
+            spell1Casts: number
+            spell2Casts: number
+            spell3Casts: number
+            spell4Casts: number
+            summoner1Casts: number
+            summoner1Id: number
+            summoner2Casts: number
+            summoner2Id: number
+            summonerId: EncryptedSummonerId
+            summonerLevel: number
+            summonerName: string
+            teamEarlySurrendered: false
+            teamId: number
+            teamPosition: string
+            timeCCingOthers: number
+            timePlayed: number
+            totalDamageDealt: number
+            totalDamageDealtToChampions: number
+            totalDamageShieldedOnTeammates: number
+            totalDamageTaken: number
+            totalHeal: number
+            totalHealsOnTeammates: number
+            totalMinionsKilled: number
+            totalTimeCCDealt: number
+            totalTimeSpentDead: number
+            totalUnitsHealed: number
+            tripleKills: number
+            trueDamageDealt: number
+            trueDamageDealtToChampions: number
+            trueDamageTaken: number
+            turretKills: number
+            turretTakedowns: number
+            turretsLost: number
+            unrealKills: number
+            visionScore: number
+            visionWardsBoughtInGame: number
+            wardsKilled: number
+            wardsPlaced: number
+            win: boolean
+        }>
+    }
+}
+
 export {
     Challenge,
     UserChallenges,
@@ -246,4 +512,6 @@ export {
     RankedData,
     championsData,
     itemsData,
+    errorResponse,
+    match,
 }
