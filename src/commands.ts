ef92da9db492b323ac8b05f525a6af9ac15fa561
patrick11514 <ja@patrick115.eu>
2022-11-08 12:30:51 +0100

    Edited number of choices

--- src/commands.ts
+++ src/commands.ts
@@ -186,6 +186,11 @@ let commands: {
                     { name: '3', value: '3' },
                     { name: '4', value: '4' },
                     { name: '5', value: '5' },
+                    { name: '6', value: '6' },
+                    { name: '7', value: '7' },
+                    { name: '8', value: '8' },
+                    { name: '9', value: '9' },
+                    { name: '10', value: '10' },
                 ],
             },
             {
