6b2b0d840077ce9f2f659090a7c38737416aa2c6
patrick11514 <ja@patrick115.eu>
2022-11-15 18:20:59 +0100

    Added type for matchData input

--- src/types/imageInputs.ts
+++ src/types/imageInputs.ts
@@ -1,4 +1,4 @@
-import { EncryptedSummonerId, QueueTypes, Ranks, Tiers } from './riotApi'
+import { EncryptedSummonerId, queues, QueueTypes, Ranks, Tiers } from './riotApi'
 
 interface profilePicture {
     username: string
@@ -38,4 +38,30 @@ interface rankedProfile {
     }>
 }
 
-export { profilePicture, rankedProfile }
+interface matchData {
+    queue: queues
+    userTeam: number
+    bans: Array<{
+        id: number
+        bans: Array<{
+            champion: number
+            pickTurn: number
+        }>
+    }>
+    wins: Array<{
+        id: number
+        win: boolean
+    }>
+    teams: Array<{
+        id: number
+        champion: number
+        summoner: string
+        role: string
+        items: number[]
+        kills: number
+        asists: number
+        deaths: number
+    }>
+}
+
+export { profilePicture, rankedProfile, matchData }
