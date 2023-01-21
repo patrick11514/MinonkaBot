8306d9761dbde24cb3ccaab86f421ae2fe58f249
Patrik Mintěl <ja@patrick115.eu>
2023-01-21 04:31:37 +0100

    Changed node version

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=/home/admin/.nvm/versions/node/v16.17.1/bin:/home/admin/.local/bin:/home/admin/.local/share/pnpm:$PATH
+PATH=/home/admin/.nvm/versions/node/v18.12.1/bin:/home/admin/.local/bin:/home/admin/.local/share/pnpm:$PATH
 while true; do
     echo "🔃 Starting..."
     pnpm start
