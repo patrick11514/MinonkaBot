9bee7687bc39a3e4664807c5b6a6adf2091dcd35
patrick11514 <ja@patrick115.eu>
2023-02-15 09:29:01 +0100

    Added new types + optional url for each ranked mode

--- src/types/LPDB.ts
+++ src/types/LPDB.ts
@@ -3,7 +3,10 @@ interface user {
     region: string
     lp: Array<{
         queue: string
+        rank: string
+        tier: string
         lp: number
+        url?: string
     }>
     lastUpdate: number
     matches: Array<{
