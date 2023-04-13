287e57cff8a6dfb142200288db664e4ba5692a28
patrick11514 <ja@patrick115.eu>
2023-04-13 11:26:56 +0200

    Live rank preparation

---
+++ src/commands/liveRank.ts
@@ -0,0 +1,46 @@
+import { ButtonInteraction, ChatInputCommandInteraction, Client, User } from 'discord.js'
+import Logger from '../lib/logger'
+import handleInteraction from '../components/core'
+import { SummonerBy } from '../types/riotApi'
+
+export default (client: Client) => {
+    let e = client.emitter
+    let l = new Logger('LiveRank', 'blue')
+
+    e.on('command', async (interaction: ChatInputCommandInteraction) => {
+        if (interaction.commandName === 'liverank') {
+            let queue = interaction.options.getString('queue', true)
+            let username = interaction.options.getString('username', false)
+            let region = interaction.options.getString('region', false)
+            let mention = interaction.options.getUser('mention', false)
+
+            liveRank(username, region, mention, interaction, queue)
+        }
+    })
+}
+
+export async function liveRank(
+    username: string | null,
+    region: string | null,
+    mention: User | null,
+    interaction: ChatInputCommandInteraction | ButtonInteraction,
+    queue: string
+) {
+    handleInteraction(
+        interaction,
+        username,
+        region,
+        mention,
+        'liveRank',
+        async function (
+            username: string,
+            region: string,
+            data: SummonerBy,
+            interaction: ChatInputCommandInteraction | ButtonInteraction,
+            queue: string
+        ) {},
+        liveRank,
+        [queue],
+        [queue]
+    )
+}
